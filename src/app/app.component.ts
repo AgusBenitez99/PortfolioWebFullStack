22a44f0dc99da9f7f561fed9ee0cc0bb9002e4b6
“Agustin <agustinnewells99@gmail.com>
2022-04-28 11:41:03 -0300

    Pasamos el proyecto a angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'index',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css', './app.circulo.css']
+  
+})
+export class AppComponent {
+  title = 'Prueba-Angular';
+}
